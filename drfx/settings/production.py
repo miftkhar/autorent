b14364480104c18b1276a6a50afe167a57bb6c38
Iftkhar Iqbal <iftkhar@vrguest.com>
2020-01-15 18:46:51 +0500

    adding static dir

--- drfx/settings/production.py
+++ drfx/settings/production.py
@@ -48,10 +48,15 @@ EMAIL_USE_TLS = True
 
 SECRET_KEY = '31&9*n_egoick%w+dv*1azf879u^^!uy4e9waib$qoje*y6c_@'
 
-STATIC_ROOT = "/home/rentacar/autorent/"
+STATIC_ROOT = "/home/rentacar/autorent/static/"
+STATIC_URL = '/static/'
+
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = "/home/rentacar/autorent/media/"
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/home/rentacar/autorent/static/',
+    '/home/rentacar/autorent/',
 ]
