ec9a2119985ae1add7355cd23ae160e1966905ee
Iftkhar Iqbal <iftkhar@vrguest.com>
2020-01-16 20:43:13 +0500

    csp settings change

--- drfx/settings/base.py
+++ drfx/settings/base.py
@@ -85,7 +85,7 @@ MIDDLEWARE = [
 ]
 
 
-CSP_DEFAULT_SRC = ("'unsafe-inline'",)
+CSP_DEFAULT_SRC = ("'self'", 'http://165.22.254.232:9000/',)
 CSP_STYLE_SRC = ("'self'",)
 CSP_SCRIPT_SRC = ("'self'",)
 CSP_IMG_SRC = ("'self'",)
