b55ea1338e4c36f113a9c5eea4da610ce20d3cf8
Iftkhar Iqbal <iftkhar@vrguest.com>
2020-01-18 00:21:24 +0500

    csp

--- drfx/settings/base.py
+++ drfx/settings/base.py
@@ -86,7 +86,7 @@ MIDDLEWARE = [
 
 
 CSP_DEFAULT_SRC = ("'self'", '165.22.254.232:9000',)
-CSP_STYLE_SRC = ("'self'", '165.22.254.232:9000',)
+CSP_STYLE_SRC = ("'self'", 'unsafe-inline', 'api.autorent.pk:9000',)
 CSP_SCRIPT_SRC = ("'self'",)
 CSP_IMG_SRC = ("'self'",)
 CSP_FONT_SRC = ("'self'",)
