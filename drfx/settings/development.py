e4da990e396a16fc92955e174882abe83daaff5f
Iftkhar Iqbal <iftkhar@vrguest.com>
2020-01-23 15:26:47 +0500

    mail settings for dev

--- drfx/settings/development.py
+++ drfx/settings/development.py
@@ -29,3 +29,10 @@ DATABASES = {
         }
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
