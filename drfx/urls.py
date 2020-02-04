5548a7c28a8ceb341fbbe432e34c3b25d51667a9
Iftkhar Iqbal <iftkhar@vrguest.com>
2020-02-04 11:32:38 +0500

    changing routes for vehicles

--- drfx/urls.py
+++ drfx/urls.py
@@ -1,17 +1,21 @@
 from django.conf import settings
-#from django.contrib import admin
-from baton.autodiscover import admin
+from django.contrib import admin
+# from baton.autodiscover import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('baton/', include('baton.urls')),
-    path('api/v1/', include('api.urls')),
+    # path('baton/', include('baton.urls')),
+    #path('rest-auth/', include('rest_auth.urls')),
+    #path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    #path('users/', include('users.urls')),
+    path('api/v1/vehicles/', include('vechiles.urls')),
+    #path('api/v1/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api/v1/auth/user/', include('rest_auth.urls')),
+    path('api/v1/user/registration/', include('rest_auth.registration.urls')),
     path('all-auth/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
