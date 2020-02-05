94e0c1d8e3edf72586cdfec2bffa79bedf09f4e0
Iftkhar Iqbal <iftkhar@vrguest.com>
2020-02-05 20:10:14 +0500

    adding csp policy

--- drfx/settings/base.py
+++ drfx/settings/base.py
@@ -79,7 +79,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'csp.middleware.CSPMiddleware',
+    'csp.middleware.CSPMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -91,13 +91,13 @@ MIDDLEWARE = [
 ]
 
 
-# CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'", 'http://api.autorent.pk:9000',)
-# CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", 'http://api.autorent.pk:9000',)
-# CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'", 'http://api.autorent.pk:9000',)
-# CSP_IMG_SRC = ("'self'",)
-# CSP_FONT_SRC = ("'self'",)
-# CSP_CONNECT_SRC = ("'self'",)
-# CSP_WORKER_SRC = ("'self'",)
+CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'", 'http://api.autorent.pk:9000',)
+CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", 'http://api.autorent.pk:9000',)
+CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'", 'http://api.autorent.pk:9000',)
+CSP_IMG_SRC = ("'self'",)
+CSP_FONT_SRC = ("'self'",)
+CSP_CONNECT_SRC = ("'self'",)
+CSP_WORKER_SRC = ("'self'",)
 
 
 ROOT_URLCONF = 'drfx.urls'
